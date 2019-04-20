6ee5816fe3d5e189387833679a61bd447c7ebc46
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-04-19 22:46:38 -0500

    create controller and routes for patients

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -1,19 +1,34 @@
-'use strict';
+"use strict";
 
-import express from 'express';
-import { getUsers, createUser, updateUser, deleteUser} from '../controllers/users';
-import { login } from '../controllers/login';
-import { authToken } from '../middlewares/auth-token';
+import express from "express";
+import {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser
+} from "../controllers/users";
+import {
+  getPatients,
+  createPatient,
+  updatePatient,
+  deletePatient
+} from "../controllers/patients";
+import { login } from "../controllers/login";
+import { authToken } from "../middlewares/auth-token";
 
 const router = express.Router();
 
 //CRUD USERS
-router.get('/users', getUsers);
-router.post('/user', createUser);
-router.put('/user/:userId', authToken, updateUser);
-router.delete('/user/:userId', deleteUser);
-
+router.get("/users", getUsers);
+router.post("/user", createUser);
+router.put("/user/:userId", authToken, updateUser);
+router.delete("/user/:userId", deleteUser);
+//CRUD PATIENTS
+router.get("/patients", getPatients);
+router.post("/patient", createPatient);
+router.put("/patient/:id", updatePatient);
+router.delete("/patient/:id", deletePatient);
 // LOGIN
-router.post('/login', login);
+router.post("/login", login);
 
 module.exports = router;
