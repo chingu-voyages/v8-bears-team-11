17a6b4f6a69e2bd1a15ac8dfebd82cce368e3db3
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-04-23 21:10:14 -0500

    refactor LayoutC to Rhooks

--- client/src/app/Layout/Layout.jsx
+++ client/src/app/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Sidebar/Sidebar";
@@ -6,46 +6,39 @@ import LayoutRouter from "./LayoutRouter";
 /* Styles */
 import "./Layout.sass";
 
-export default class Layout extends Component {
-  state = {
-    sidebarPos: 2
-  };
+function Layout() {
+  const [sidebarPos, setSidebarPos] = useState(2);
 
-  handleSidebar = () => {
-    if (this.state.sidebarPos < 2) {
-      this.setState(state => ({
-        sidebarPos: state.sidebarPos + 1
-      }));
+  const handleSidebar = () => {
+    if (sidebarPos < 2) {
+      setSidebarPos(sidebarPos + 1);
     } else {
-      this.setState({
-        sidebarPos: 0
-      });
+      setSidebarPos(0);
     }
   };
 
-  render() {
-    let sidebarClass = ["sidebar"];
-    let mainClass = ["main"];
-
-    if (this.state.sidebarPos === 1) {
-      sidebarClass.push("sidebarMin");
-      mainClass.push("mainMin");
-    }
-    if (this.state.sidebarPos === 2) {
-      sidebarClass.push("sidebarOpen");
-      mainClass.push("mainOpen");
-    }
-
-    return (
-      <div className="layout">
-        <Navbar onChange={this.handleSidebar} />
-        <div className={sidebarClass.join(" ")}>
-          <Sidebar />
-        </div>
-        <div className={mainClass.join(" ")}>
-          <LayoutRouter />
-        </div>
-      </div>
-    );
+  let sidebarClass = ["sidebar"];
+  let mainClass = ["main"];
+  if (sidebarPos === 1) {
+    sidebarClass.push("sidebarMin");
+    mainClass.push("mainMin");
   }
+  if (sidebarPos === 2) {
+    sidebarClass.push("sidebarOpen");
+    mainClass.push("mainOpen");
+  }
+
+  return (
+    <div className="layout">
+      <Navbar onChange={handleSidebar} />
+      <div className={sidebarClass.join(" ")}>
+        <Sidebar />
+      </div>
+      <div className={mainClass.join(" ")}>
+        <LayoutRouter />
+      </div>
+    </div>
+  );
 }
+
+export default Layout;
