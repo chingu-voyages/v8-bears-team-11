648396c10c73571c8a16cf8e996a6aa175f2d253
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-05-05 00:19:42 -0500

    Updates

---
+++ precache-manifest.a65ff5e7c7192e2e531a9ebc67eeec2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da0c8f72f9cb4e87bc34",
+    "url": "/v8-bears-team-11/static/js/runtime~main.6ba797df.js"
+  },
+  {
+    "revision": "abe55538ebbb1c6d404d",
+    "url": "/v8-bears-team-11/static/js/main.e8d662a3.chunk.js"
+  },
+  {
+    "revision": "6516d0effa10f3b32c28",
+    "url": "/v8-bears-team-11/static/js/2.f95dda02.chunk.js"
+  },
+  {
+    "revision": "abe55538ebbb1c6d404d",
+    "url": "/v8-bears-team-11/static/css/main.2b8d2aa8.chunk.css"
+  },
+  {
+    "revision": "6516d0effa10f3b32c28",
+    "url": "/v8-bears-team-11/static/css/2.c7ce11e7.chunk.css"
+  },
+  {
+    "revision": "d34bb9c7b6fbba74e8bee70f8c275793",
+    "url": "/v8-bears-team-11/index.html"
+  }
+];
